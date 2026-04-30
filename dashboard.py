dde0f0ac2c712670bb826998cfebfc0d5e26100f
AdielAdornAdronAxleAxelAlex <167777106+AdielAdornAdronAxleAxelAlex@users.noreply.github.com>
2026-04-30 11:25:40 +0700

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,31 +3,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-weekday_map = {
-    0: "Monday",
-    1: "Tuesday",
-    2: "Wednesday",
-    3: "Thursday",
-    4: "Friday",
-    5: "Saturday",
-    6: "Sunday"
-}
+all_df = pd.read_csv("processed.csv")
 
-def create_sumbyweekday_df(df):
-    byday_df=df.drop(columns=['dteday'])
-    byday_df=byday_df.groupby(['weekday']).sum()
-    return byday_df
-
-all_df = pd.read_csv("data/day.csv")
-
-datetime_columns = ["dteday"]
+datetime_columns = ["date"]
 
 for column in datetime_columns:
     all_df[column] = pd.to_datetime(all_df[column])
 
 # Filter data
-min_date = all_df["dteday"].min()
-max_date = all_df["dteday"].max()
+min_date = all_df["date"].min()
+max_date = all_df["date"].max()
 
 with st.sidebar:
     # Menambahkan logo perusahaan
@@ -40,153 +25,96 @@ with st.sidebar:
         value=[min_date, max_date]
     )
 
-main_df = all_df[(all_df["dteday"] >= str(start_date)) & 
-                (all_df["dteday"] <= str(end_date))]
+main_df = all_df[(all_df["date"] >= str(start_date)) & 
+                (all_df["date"] <= str(end_date))]
 
-# # Menyiapkan berbagai dataframe
-sum_by_weekday_df = create_sumbyweekday_df(main_df)
-
-# plot number of daily orders (2021)
 st.header('Bike Rental Dashboard :sparkles:')
-st.subheader('Daily rentals')
+st.subheader('weather factor correlation')
 
-col1, col2, col3=st.columns(3)
+weather_factor = st.selectbox(
+    'Factor?',
+    ('binned_temperature','binned_humidity','binned_wind_speed','binned_air_temperature','season','weather'))
+
+binned_order=['very low','low','average','high','very high']
+season_order=['spring','summer','fall','winter']
+weather_order=['clear','light rain/snow','misty','heavy rain']
+
+if weather_factor in ['binned_temperature','binned_humidity',
+                      'binned_wind_speed','binned_air_temperature']:
+    main_df[weather_factor]=pd.Categorical(
+        main_df[weather_factor],
+        categories=binned_order,
+        ordered=True
+    )
+
+elif weather_factor=='season':
+    main_df[weather_factor]=pd.Categorical(
+        main_df[weather_factor],
+        categories=season_order,
+        ordered=True
+    )
+
+elif weather_factor=='weather':
+    main_df[weather_factor]=pd.Categorical(
+        main_df[weather_factor],
+        categories=weather_order,
+        ordered=True
+    )
+
+weather_df=main_df.groupby(weather_factor, sort=False)['total'].mean().reset_index().sort_values(weather_factor)
+col1,col2=st.columns(2)
 
 with col1:
-    total_rentals = main_df.cnt.sum()
+    total_rentals = main_df.total.sum()
     st.metric("Total rentals", value=total_rentals)
 
 with col2:
-    total_casual_customers= main_df.casual.sum()
-    st.metric("Total casual customers", value=total_casual_customers)
+    average_rentals = main_df.total.mean().round(2)
+    st.metric("Average rentals", value=average_rentals)
 
-with col3:
-    total_registered_customers= main_df.registered.sum()
-    st.metric("Total registered customers", value=total_registered_customers)
-
-col4, col5, col6= st.columns(3)
-
-with col4:
-    average_rentals = main_df.cnt.mean().round(2)
-    st.metric("Average total rentals", value=average_rentals)
-
-with col5:
-    average_casual_customers= main_df.casual.mean().round(2)
-    st.metric("Average casual customers", value=average_casual_customers)
-
-with col6:
-    average_registered_customers= main_df.registered.mean().round(2)
-    st.metric("Average registered customers", value=average_registered_customers)
-
-#daily performance
+#weather factor correlation
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.set_title("Number of daily rentals", loc="center", fontsize=30)
-ax.plot(
-    main_df["dteday"],
-    main_df["cnt"],
-    marker='o', 
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax.bar(weather_df[weather_factor], weather_df['total'], color="#90CAF9")
+ax.axhline(average_rentals, color='red', linestyle='--', label='rata-rata')
+ax.set_title(f'Dampak {weather_factor} pada total', fontsize=20)
+ax.set_xlabel(weather_factor,fontsize=20)
+ax.set_ylabel(f'rata-rata total',fontsize=20)
+ax.tick_params(axis='x', rotation=45,labelsize=20)
+ax.tick_params(axis='y',labelsize=20)
+ax.legend()
 st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(16, 8))
-ax.set_title("Number of daily casual rentals", loc="center", fontsize=30)
-ax.plot(
-    main_df["dteday"],
-    main_df["casual"],
-    marker='o', 
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-st.pyplot(fig)
+#work/holiday correlation
+day_factor = st.selectbox(
+    'Working_day or holiday?',
+    ('working_day','holiday'))
+
+customer_type = st.selectbox(
+    'Registered or casual?',
+    ('registered','casual'))
+
+day_df=main_df.groupby(day_factor)[customer_type].mean().reset_index()
+
+col1,col2=st.columns(2)
+
+with col1:
+    total_specific_rentals = main_df[customer_type].sum()
+    st.metric(label=f'Total {customer_type} rentals', value=total_specific_rentals)
+
+with col2:
+    average_specific_rentals = main_df[customer_type].mean().round(2)
+    st.metric(label=f'Average {customer_type} rentals', value=average_specific_rentals)
+
 
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.set_title("Number of daily registered rentals", loc="center", fontsize=30)
-ax.plot(
-    main_df["dteday"],
-    main_df["registered"],
-    marker='o', 
-    linewidth=2,
-    color="#90CAF9"
-)
+ax.bar(day_df[day_factor], day_df[customer_type], color="#90CAF9")
+ax.axhline(average_specific_rentals, color='red', linestyle='--', label='rata-rata')
+ax.set_title(f'Dampak {day_factor} pada {customer_type}', fontsize=28)
+ax.set_xlabel(day_factor,fontsize=20)
+ax.set_ylabel(f'rata-rata total',fontsize=20)
+ax.tick_params(axis='x', rotation=45,labelsize=20)
 ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-st.pyplot(fig)
-
-# weekday performance  
-sum_by_weekday_df = sum_by_weekday_df.reset_index()
-sum_by_weekday_df["weekday_name"] = sum_by_weekday_df["weekday"].map(weekday_map)
-
-max_idx=sum_by_weekday_df["cnt"].idxmax()
-
-st.subheader("Rentals per weekday")
-
-fig, ax = plt.subplots(figsize=(20, 10))
-
-colors=["#D3D3D3"]*len(sum_by_weekday_df)
-colors[list(sum_by_weekday_df.index).index(max_idx)]="#90CAF9"
-
-sns.barplot(
-    x="cnt", 
-    y="weekday_name",
-    orient='h',
-    data=sum_by_weekday_df,
-    palette=colors,
-    ax=ax
-)
-
-ax.set_title("Number of total rentals per weekday", loc="center", fontsize=30)
-ax.set_ylabel(None)
-ax.set_xlabel(None)
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-st.pyplot(fig)
-
-fig, ax = plt.subplots(figsize=(20, 10))
-
-colors=["#D3D3D3"]*len(sum_by_weekday_df)
-colors[list(sum_by_weekday_df.index).index(max_idx)]="#90CAF9"
-
-sns.barplot(
-    x="casual", 
-    y="weekday_name",
-    orient='h',
-    data=sum_by_weekday_df,
-    palette=colors,
-    ax=ax
-)
-
-ax.set_title("Number of casual rentals per weekday", loc="center", fontsize=30)
-ax.set_ylabel(None)
-ax.set_xlabel(None)
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-st.pyplot(fig)
-
-fig, ax = plt.subplots(figsize=(20, 10))
-
-colors=["#D3D3D3"]*len(sum_by_weekday_df)
-colors[list(sum_by_weekday_df.index).index(max_idx)]="#90CAF9"
-
-sns.barplot(
-    x="registered", 
-    y="weekday_name",
-    orient='h',
-    data=sum_by_weekday_df,
-    palette=colors,
-    ax=ax
-)
-
-ax.set_title("Number of registered rentals per weekday", loc="center", fontsize=30)
-ax.set_ylabel(None)
-ax.set_xlabel(None)
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax.legend(fontsize=20)
 st.pyplot(fig)
 
 
